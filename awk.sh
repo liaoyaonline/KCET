e747737d6504dba82088b56118a9c26215e9d760
liaoyaonline <71174500014@stu.ecnu.edu.cn>
2019-11-25 13:03:44 +0800

    change

--- awk.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -s 'http://dict.youdao.com/search?q='$keyword'' | awk 'BEGIN{j=0;i=0;} {if(/phrsListTab/){i++;} if(i==1){print $0; if(/<\/ul>/){i=0;}} if(/collinsToggle/){ j++;} if(j==1) {print $0; if(/<\/ul>/){j=0;}}}' > $TEM_FILE
--- ydtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while read line
-do
-    var=$(yd $line)
-    echo "$var" >> chinese.txt
-done < english.txt
